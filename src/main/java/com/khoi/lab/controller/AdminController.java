e7ceba9b70c5b6dc0daed370c8483638e0b66a18
lekhoi <kxyz207@gmail.com>
2025-07-08 16:23:51 +0700

    feat: account management actions
    - Added enable/disable account action
    - Added convert to USER/ADMIN action
    - Added account edit request object
    - Changed date formatting to dd/mm/yyyy

--- src/main/java/com/khoi/lab/controller/AdminController.java
+++ src/main/java/com/khoi/lab/controller/AdminController.java
@@ -4,14 +4,15 @@ import org.springframework.stereotype.Controller;
 
 import com.khoi.lab.dao.AccountDAO;
 import com.khoi.lab.entity.Account;
+import com.khoi.lab.object.AccountEditRequest;
 
 import jakarta.servlet.http.HttpSession;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @Controller
 @RequestMapping("/admin")
@@ -55,6 +56,19 @@ public class AdminController {
     }
 
     /**
+     * Redirect to admin/accounts page
+     * 
+     * @param param
+     * @return
+     */
+    @GetMapping("/accounts/edit")
+    public ModelAndView accountEdit() {
+        ModelAndView mav = new ModelAndView("admin/accounts");
+        mav.addObject("accounts", accountDAO.accountList());
+        return mav;
+    }
+
+    /**
      * Handle account edit request
      * 
      * @param id
@@ -63,26 +77,18 @@ public class AdminController {
      * @return
      */
     @PostMapping("/accounts/edit")
-    public ModelAndView accountsEditRequest(
-            @RequestParam Long id,
-            @RequestParam(required = false) String isDisabled,
-            @RequestParam(required = false) String isAdmin) {
-        Account account = accountDAO.accountFindWithId(id);
+    public ModelAndView accountsEditRequest(@RequestBody AccountEditRequest request) {
+        Account account = accountDAO.accountFindWithId(request.id);
 
-        System.out.println("isDisabled: " + isDisabled);
-        System.out.println("isAdmin: " + isAdmin);
-
-        if (isDisabled != null) {
-            System.out.println("| Edited " + account + ", isDisabled = " + isDisabled);
-            account.setDisabled(isDisabled.equals("true"));
-            accountDAO.accountUpdate(account);
+        if (request.isDisabled != null) {
+            account.setDisabled(request.isDisabled.equals("true"));
         }
 
-        if (isAdmin != null) {
-            System.out.println("| Edited " + account + ", isAdmin = " + isAdmin);
-            account.setDisabled(isAdmin.equals("true"));
-            accountDAO.accountUpdate(account);
+        if (request.isAdmin != null) {
+            account.setAdmin(request.isAdmin.equals("true"));
         }
+
+        accountDAO.accountUpdate(account);
 
         ModelAndView mav = new ModelAndView("admin/accounts");
         mav.addObject("accounts", accountDAO.accountList());
---
+++ src/main/java/com/khoi/lab/object/AccountEditRequest.java
@@ -0,0 +1,10 @@
+package com.khoi.lab.object;
+
+/**
+ * Account Edit Request Object
+ */
+public class AccountEditRequest {
+    public Long id;
+    public String isAdmin;
+    public String isDisabled;
+}
