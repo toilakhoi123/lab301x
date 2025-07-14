e4e555af8103c40f5b7b9b107d23c9f7510e8dc0
lekhoi <kxyz207@gmail.com>
2025-07-14 20:23:57 +0700

    chore: removed accountDAO import

--- src/main/java/com/khoi/lab/controller/GeneralController.java
+++ src/main/java/com/khoi/lab/controller/GeneralController.java
@@ -4,19 +4,14 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
-import com.khoi.lab.dao.AccountDAO;
-
 @Controller
 public class GeneralController {
-    private final AccountDAO accountDAO;
-
     /**
      * DAO Initiator
      * 
      * @param accountDAO
      */
-    public GeneralController(AccountDAO accountDAO) {
-        this.accountDAO = accountDAO;
+    public GeneralController() {
     }
 
     /**
