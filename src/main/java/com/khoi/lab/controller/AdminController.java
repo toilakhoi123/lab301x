a8fdf4f336dd7223e2cb7ecf9fcf8a1fee4c2c85
lekhoi <kxyz207@gmail.com>
2025-08-13 11:50:42 +0700

    chore: minor cleaning

--- src/main/java/com/khoi/lab/controller/AdminController.java
+++ src/main/java/com/khoi/lab/controller/AdminController.java
@@ -32,12 +32,12 @@ import com.khoi.lab.service.UserPermissionService;
 
 import jakarta.servlet.http.HttpSession;
 
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.servlet.ModelAndView;
 
 /**
  * Controller for admin mappings
