f42fd06faa46f5edbb51827aa0e15a102e9ef35f
lekhoi <kxyz207@gmail.com>
2025-08-23 16:36:20 +0700

    feat: gitignore file

--- src/main/java/com/khoi/lab/entity/Account.java
+++ src/main/java/com/khoi/lab/entity/Account.java
@@ -113,6 +113,10 @@ public class Account {
         return id;
     }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
+
     public String getUsername() {
         return username;
     }
